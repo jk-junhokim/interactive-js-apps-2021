a7fd75b4b3da5ad77e7b73fb36b835f3beb82528
Junho Kim <89207813+jk-junhokim@users.noreply.github.com>
2021-11-15 14:36:39 +0000

    to do list part 1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
+import React from 'react';
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div>New React App</div>
-  );
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    const onChange = (event) => setToDo(event.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault(); // stops from refreshing webpage
+        if (toDo === "") {
+            return;
+        }
+        setToDos((currentArray) => [...currentArray, toDo]);
+        setToDo(""); // passing empty string as argument through setToDo function
+    }
+    console.log(toDos);
+
+    return (
+        <div>
+            <h2>My To Dos : ({toDos.length})</h2>
+            <form onSubmit={onSubmit}>
+                <input 
+                    onChange={onChange} 
+                    value={toDo} 
+                    type="text" 
+                    placeholder="Write your to do..." 
+                />
+                <button>Add To Do</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
