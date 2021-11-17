134ab7b30a585ad6fd24cc648da451015dbab446
Junho Kim <89207813+jk-junhokim@users.noreply.github.com>
2021-11-17 14:00:22 +0000

    coin tracker part 1

--- my-app/src/CoinTracker.js
+++ my-app/src/CoinTracker.js
@@ -1,13 +1,25 @@
-import './ToDoList.css';
+import './CoinTracker.css';
 import React from 'react';
 import { useState, useEffect } from "react";
 
 function CoinTracker(){
+    const [loading, setLoading] = useState("true");
+    const [coins, setCoins] = useState([]);
+    useEffect(() => {
+        fetch("https://api.coinpaprika.com/v1/tickers")
+        .then((response) => response.json())
+        .then((json) => setCoins(json));
+        setLoading(false);
+    }, []);
     return(
         <div>
             <h1>Coin Tracker 2021</h1>
+            <h3>Total number of coins: ({coins.length})</h3>
+            {loading ? <strong>Loading...</strong> : null}
+            <ul>
+            {coins.map((coin) => <li>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD</li>)}
+            </ul>
         </div>
-
     );
 }
 
