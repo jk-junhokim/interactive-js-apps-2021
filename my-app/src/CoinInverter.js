d9e42efd86f9ffec6426a71cd65997623eebdc53
Junho Kim <89207813+jk-junhokim@users.noreply.github.com>
2021-11-17 14:35:49 +0000

    coin inverter part 2

--- my-app/src/CoinInverter.js
+++ my-app/src/CoinInverter.js
@@ -2,9 +2,16 @@ import './CoinInverter.css';
 import React from 'react';
 import { useState, useEffect } from "react";
 
+/* 
+https://www.robinwieruch.de/react-event-handler/
+*/
+
 function CoinInverter(){
     const [loading, setLoading] = useState("true");
     const [coins, setCoins] = useState([]);
+    const onChange = (event) => {
+        console.log(event.target.value);
+    }
     useEffect(() => {
         fetch("https://api.coinpaprika.com/v1/tickers")
         .then((response) => response.json())
@@ -13,12 +20,15 @@ function CoinInverter(){
     }, []);
     return(
         <div>
-            <h1>Coin Tracker 2021</h1>
+            <h1>Coin Inverter 2021</h1>
             <h3>Total number of coins: ({coins.length})</h3>
-            {loading ? <strong>Loading...</strong> : null}
-            <ul>
-            {coins.map((coin) => <li>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD</li>)}
-            </ul>
+            {loading ? <strong>Loading...</strong> : (
+                <select value={coin} onChange={onChange}>
+                    {coins.map((coin) => 
+                        <option>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD</option>
+                    )}
+                </select>
+            )}
         </div>
     );
 }
