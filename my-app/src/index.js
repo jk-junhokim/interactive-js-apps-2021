a6b51f72a0f3be138fc0626a308d6cfb50caac33
Junho Kim <89207813+jk-junhokim@users.noreply.github.com>
2021-11-17 14:20:17 +0000

    coin inverter part 1

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import ToDoList from './ToDoList';
-import CoinTracker from './CoinTracker';
+// import CoinTracker from './CoinTracker';
+import CoinInverter from './CoinInverter';
 
+/* Create To Do List */
 // ReactDOM.render(<React.StrictMode><ToDoList /></React.StrictMode>, document.getElementById('root'));
-ReactDOM.render(<React.StrictMode><CoinTracker /></React.StrictMode>, document.getElementById('root'));
+
+/* Create Coin Tracker */
+// ReactDOM.render(<React.StrictMode><CoinTracker /></React.StrictMode>, document.getElementById('root'));
+
+/* Create Coin Inverter */
+ReactDOM.render(<React.StrictMode><CoinInverter /></React.StrictMode>, document.getElementById('root'));
 
